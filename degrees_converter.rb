8e3ee7fdb3b84e88e8a01d8c4be7e4e7c27f78c0
Vitaliy <vitaliy199811@gmail.com>
2020-09-29 14:48:42 +0700

    Add class DegreesConverter

---
+++ degrees_converter.rb
@@ -0,0 +1,39 @@
+class DegreeConverter
+    def self.convert(value, from, to)
+        case from
+        when 'C'
+            convert_from_c(value, to)
+        when 'K'
+            convert_from_k(value, to)
+        when 'F'
+            convert_from_f(value, to)
+        end
+    end
+
+    def self.convert_from_c(value, to)
+        case to
+        when 'F'
+            ((value * 9) / 5) + 32
+        when 'K'
+            value + 273
+        end
+    end
+
+    def self.convert_from_k(value, to)
+        case to
+        when 'F'
+            ((value * 9) / 5) - 495
+        when 'C'
+            value - 273
+        end
+    end
+
+    def self.convert_from_f(value, to)
+        case to
+        when 'C'
+            ((value - 32) * 5) / 9
+        when 'K'
+            ((value + 459) * 5) / 9
+        end
+    end
+end
